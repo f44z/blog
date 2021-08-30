bf4a844b0782983b90a761231b7f58eb96af1879
f44z <vgood@protonmail.com>
2021-08-30 21:30:23 +0200

    Remove disquss - fix.

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   title: 'f44z',
   subtitle: 'passionate about offensive security',
   copyright: '© All rights reserved.',
-  disqusShortname: 'f44z',
+  disqusShortname: '',
   postsPerPage: 4,
   googleAnalyticsId: 'UA-73379983-2',
   useKatex: false,
